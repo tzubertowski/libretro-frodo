09b988244b86e7c3b08e7c8c55566f0a24aa2103
twinaphex <libretro@gmail.com>
2021-11-05 05:15:21 +0100

    Cleanups

--- libretro/gui/utype.h
+++ libretro/gui/utype.h
@@ -5,27 +5,31 @@
 #ifndef HATARI_UTYPE_H
 #define HATARI_UTYPE_H
 
-typedef unsigned char	Uint8;
-typedef signed char	Sint8;
-typedef unsigned short	Uint16;
-typedef signed short	Sint16;
-typedef unsigned int	Uint32;
-typedef signed int	Sint32;
-
-typedef signed char     int8;
-typedef signed short    int16;
-typedef signed int   int32;
-typedef unsigned char   uint8;
-typedef unsigned short  uint16;
-typedef unsigned int  uint32;
-
+#include <stdint.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#ifdef _WIN32
+#include <direct.h>
+#else
 #include <unistd.h>
+#endif
 
-//RETO HACK
-#ifdef RETRO
 #include <ctype.h>
+
+typedef uint8_t	 Uint8;
+typedef int8_t	 Sint8;
+typedef uint16_t Uint16;
+typedef int16_t	 Sint16;
+typedef uint32_t Uint32;
+typedef int32_t	 Sint32;
+
+typedef int8_t     int8;
+typedef int16_t    int16;
+typedef int32_t   int32;
+typedef uint8_t   uint8;
+typedef uint16_t  uint16;
+typedef uint32_t  uint32;
+
 
 #if defined (__vita__) || defined(__psp__)
 #define	getcwd(a,b)	"/"
@@ -37,7 +41,6 @@ typedef unsigned int  uint32;
 #include <cell.h>
 #include <string.h>
 #define	getcwd(a,b)	"/"
-#include <unistd.h> //stat() is defined here
 #define S_ISDIR(x) (x & CELL_FS_S_IFDIR)
 #define F_OK 0
 #define ftello ftell
@@ -45,14 +48,11 @@ typedef unsigned int  uint32;
 #define getenv(a)	"/dev_hdd0/HOMEBREW/ST/"
 #define tmpfile() NULL
 #endif
-//#define PROG_NAME "Hatari devel (" __DATE__ ")"
-#endif
 
-#ifdef WIN32
+#ifdef _WIN32
 #define PATHSEP '\\'
 #else
 #define PATHSEP '/'
 #endif
 
 #endif
-
